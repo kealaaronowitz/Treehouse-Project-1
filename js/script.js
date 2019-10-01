6fda80b36adb455da0c729cd493b8312e83992a9
kealaaronowitz <53239000+kealaaronowitz@users.noreply.github.com>
2019-09-30 22:25:13 -0700

    Added printQuote function

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ let quotes = [
     citation: "www.ftw.usatoday.org"
   },
   {
-    quote: "Andre Dawson has a bruised knee and is listed as day-to-day (pause). Arent we all",
+    quote: "Andre Dawson has a bruised knee and is listed as day-to-day (pause). Aren't we all?",
     source: "Vin Scully",
     citation: "On-Air Radio Broadcast",
     year: 1991
@@ -49,7 +49,7 @@ let quotes = [
     source: "Tommy Lasorda"
   },
   {
-    quote: "If Casey Stangel were alive today, he would be spinning in his grave",
+    quote: "If Casey Stengel were alive today, he would be spinning in his grave.",
     source: "Ralph Kiner",
     citation: "www.quotes.net",
     year: 1976
@@ -63,7 +63,7 @@ let quotes = [
 ***/
 
 function getRandomQuote(){
-  var randomNumber = Math.floor(Math.random() * quotes.length);
+let randomNumber = Math.floor(Math.random() * quotes.length);
   return quotes[randomNumber];
 }
 
@@ -80,8 +80,22 @@ function getRandomQuote(){
    - Set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
 
-
-
+function printQuote(){
+  let randomQuotes; 
+  randomQuotes = getRandomQuote();
+  let htmlString;
+  htmlString = " ";
+  htmlString += '<p class="quote">' + randomQuotes.quote + '</p>';
+  htmlString += '<p class="source">' + randomQuotes.source;
+  if (randomQuotes.citation) {
+    htmlString += '<span class="citation">' + randomQuotes.citation + '</span>';
+    }
+  if (randomQuotes.year) {
+    htmlString += '<span class="year">' + randomQuotes.year + '</span>';
+  }
+  htmlString += '</p>';
+  document.getElementById('quote-box').innerHTML = htmlString;
+}
 
 /***
   When the "Show another quote" button is clicked, the event listener 
